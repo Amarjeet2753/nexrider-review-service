be832d0db411cd1f2e4f886c068fb42632be1459
Amarjeet Prajapati <amarjeet275303@gmail.com>
2026-02-22 18:16:20 +0530

    initial setup

---
+++ src/main/java/com/example/nexridereviewservice/NexRideReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.nexridereviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexRideReviewServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexRideReviewServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nexridereviewservice/NexRideReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nexridereviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexRideReviewServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
